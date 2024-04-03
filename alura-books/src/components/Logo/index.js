fd8e3f3384aa994ac992b15644f129959b90b609
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-04-02 23:29:01 -0300

    Usando loops

--- alura-books/src/components/Logo/index.js
+++ alura-books/src/components/Logo/index.js
@@ -1,10 +1,15 @@
+import './style.css'
 import logo from '../../img/logo.svg'
 
 function Logo() {
     return (
         <div className="logo">
-          <img src={logo} alt="Logo Alura Books"></img>
-          <p><strong>Alura</strong>Books</p>
+            <img
+                src={logo} 
+                alt="Logo Alura Books" 
+                className="logo-img">
+            </img>
+            <p><strong>Alura</strong>Books</p>
         </div>
     )
 }

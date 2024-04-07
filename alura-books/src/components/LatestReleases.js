a972ddf7c5af2fa051318fa03a3989b08f024327
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-04-06 21:04:21 -0300

    Card reutilizável com Props

--- alura-books/src/components/LatestReleases.js
+++ alura-books/src/components/LatestReleases.js
@@ -1,5 +1,7 @@
 import { books } from '../latestReleasesData'
 import { Title } from './Title'
+import RecommendationCard from './RecommendationCard'
+import bookImage from '../img/book2.png'
 import styled from 'styled-components'
 
 const LatestReleasesContainer = styled.section`
@@ -20,15 +22,23 @@ const NewBooksContainer = styled.div`
 function LatestReleases() {
     return (
         <LatestReleasesContainer>
-            <Title 
-                color="#EB9B00" 
+            <Title
+                color="#EB9B00"
                 fontSize="36px"
-            >ÚLTIMOS LANÇAMENTOS</Title>
+            >
+                ÚLTIMOS LANÇAMENTOS
+            </Title>
             <NewBooksContainer>
                 {books.map(book => (
                     <img src={book.src} alt={`Capa do livro ${book.name}`} />
                 ))}
             </NewBooksContainer>
+            <RecommendationCard 
+                title="Talvez você se interesse por"
+                subtitle="Angular 11"
+                description="Construindo uma aplicação com a plataforma Google"
+                image={bookImage}
+            />
         </LatestReleasesContainer>
     )
 }
